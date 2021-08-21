6a11dd475e4a36afdd1aaa4542c80488faa94170
gari8 <hagari.hayato.infratop@gmail.com>
2021-08-21 20:48:20 +0900

    commit

--- @python/Atcoder/Weekly/B214B/main.py
+++ @python/Atcoder/Weekly/B214B/main.py
@@ -1,6 +1,12 @@
 import sys
 input = sys.stdin.readline
-a,b = map(int, input().split())
+s,t = map(int, input().split())
 
 if __name__ == '__main__':
-    print(a^b)
+    cnt = 0
+    for i in range(s+2):
+        for j in range(s+2):
+            for k in range(s+2):
+                if i+j+k <= s and i*j*k <= t:
+                    cnt += 1
+    print(cnt)
--- @python/Atcoder/Weekly/B214C/main.py
+++ @python/Atcoder/Weekly/B214C/main.py
@@ -1,6 +1,22 @@
 import sys
 input = sys.stdin.readline
-a,b = map(int, input().split())
+n = int(input())
+s = list(map(int, input().split()))
+t = list(map(int, input().split()))
 
 if __name__ == '__main__':
-    print(a^b)
+    cnt = 0
+    ans = []
+    for i in range(n):
+        if i == 0:
+            ans.append(t[i])
+            cnt = t[i]+s[i]
+            continue
+        if t[i] < cnt:
+            ans.append(t[i])
+            cnt = t[i]+s[i]
+        else:
+            ans.append(cnt)
+            cnt = s[i]+cnt
+    for i in range(n):
+        print(ans[i])
