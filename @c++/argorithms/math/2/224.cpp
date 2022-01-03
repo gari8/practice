a27598d41abef8ba7130c750331ef4d84be147d5
gari8 <hagari.hayato.infratop@gmail.com>
2022-01-04 01:33:24 +0900

    feat/commit

---
+++ @c++/argorithms/math/2/224.cpp
@@ -0,0 +1,15 @@
+//
+// Created by HAGARI HAYATO on 2022/01/04.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << A*B*C << endl;
+    return 0;
+}
